f708e603ecaf7013bef893056d22a12f400d585f
Julesntizimira <ntizimijules5@gmail.com>
2024-02-01 00:32:11 +0200

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,24 +8,25 @@ def validUTF8(data):
         represents a valid UTF-8 encoding
     '''
     binary_list = [format(decimal_number, '08b') for decimal_number in data]
-    for i in range(len(binary_list)):
+    i = 0
+    while i < len(binary_list):
         byt = binary_list[i]
         if byt.startswith('0'):
-            continue
+            i = i + 1
         elif byt.startswith('110'):
             if not binary_list[i + 1].startswith('10'):
                 return False
-            i = i + 1
+            i = i + 2
         elif byt.startswith('1110'):
             for j in range(i + 1, i + 3):
                 if not binary_list[j].startswith('10'):
                     return False
-            i = i + 2
+            i = i + 3
         elif byt.startswith('11110'):
             for j in range(i + 1, i + 4):
                 if not binary_list[j].startswith('10'):
                     return False
-            i = i + 3
+            i = i + 4
         else:
             return False
     return True
