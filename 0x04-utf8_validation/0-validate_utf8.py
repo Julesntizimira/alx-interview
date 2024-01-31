35e89cb62b35370960aa4a44185fc269feb53880
Julesntizimira <ntizimijules5@gmail.com>
2024-02-01 01:13:44 +0200

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,7 +7,8 @@ def validUTF8(data):
     ''' determines if a given data set
         represents a valid UTF-8 encoding
     '''
-    if not isinstance(data, list):
+    if not isinstance(data, list) or\
+            not all([isinstance(n, int) for n in data]):
         return False
     binary_list = [format(decimal_number, '08b') for decimal_number in data]
     i = 0
