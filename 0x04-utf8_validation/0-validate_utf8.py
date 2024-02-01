43820c0c6ca7538a29a71047f17200aa86fa6dd6
Julesntizimira <ntizimijules5@gmail.com>
2024-02-01 07:57:23 +0200

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 ''' UTF-8 Validation
 '''
+from typing import List
 
 
-def validUTF8(data):
+def validUTF8(data: List[int]) -> bool:
     ''' determines if a given data set
         represents a valid UTF-8 encoding
     '''
     if not isinstance(data, list) or\
             not all([isinstance(n, int) for n in data]):
+        return False
+    if not all([n <= 256 for n in data]):
         return False
     binary_list = [format(decimal_number, '08b') for decimal_number in data]
     i = 0
