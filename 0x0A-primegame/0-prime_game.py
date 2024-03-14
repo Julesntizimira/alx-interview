4ef2f0f250bbd2c42ee2acf5a0d67bc284d7e952
Julesntizimira <ntizimijules5@gmail.com>
2024-03-14 18:56:11 +0200

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -20,34 +20,38 @@ def isWinner(x, nums):
                     if x % number == 0 and x >= (number * number):
                         primes.remove(x)
             e_cache_primes[query_num] = primes
-        if len(primes) == 0:
+        if len(primes) % 2 == 0:
             wins['Ben'] += 1
-            continue
-        turn = 'Maria'
-        num_set = [x for x in range(2, query_num + 1)]
-        for k in range(len(primes)):
-            if k != 0:
-                if turn == 'Maria':
-                    turn = 'Ben'
-                else:
-                    turn = 'Maria'
-            chosen_num = primes[0]
-            primes.remove(chosen_num)
-            for num in num_set:
-                if num % chosen_num == 0:
-                    num_set.remove(num)
-            single_wins[turn] += 1
-            if len(num_set) == 0 or len(primes) == 0:
-                single_wins[turn] += 1
-                break
-        if single_wins['Maria'] == single_wins['Ben']:
-            continue
-        winner = 'Maria' if single_wins['Maria'] >\
-            single_wins['Ben'] else 'Ben'
-        wins[winner] += 1
-        if i + 1 < x - 1 and i + 1 > len(nums):
-            wins[winner] += 1
-            break
+        else:
+            wins['Maria'] += 1
+    #     if len(primes) == 0:
+    #         wins['Ben'] += 1
+    #         continue
+    #     turn = 'Maria'
+    #     num_set = [x for x in range(2, query_num + 1)]
+    #     for k in range(len(primes)):
+    #         if k != 0:
+    #             if turn == 'Maria':
+    #                 turn = 'Ben'
+    #             else:
+    #                 turn = 'Maria'
+    #         chosen_num = primes[0]
+    #         primes.remove(chosen_num)
+    #         for num in num_set:
+    #             if num % chosen_num == 0:
+    #                 num_set.remove(num)
+    #         single_wins[turn] += 1
+    #         if len(num_set) == 0 or len(primes) == 0:
+    #             single_wins[turn] += 1
+    #             break
+    #     if single_wins['Maria'] == single_wins['Ben']:
+    #         continue
+    #     winner = 'Maria' if single_wins['Maria'] >\
+    #         single_wins['Ben'] else 'Ben'
+    #     wins[winner] += 1
+    #     if i + 1 < x - 1 and i + 1 > len(nums):
+    #         wins[winner] += 1
+    #         break
     if wins['Maria'] == wins['Ben']:
         return None
     return 'Maria' if wins['Maria'] > wins['Ben'] else 'Ben'
